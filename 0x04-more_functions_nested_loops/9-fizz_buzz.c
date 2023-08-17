bb542c0c918610cc408069b354791d4af2c83a33
SegunEmmanuel1 <segunemmanuel67@yahoo.com>
2023-08-17 12:13:07 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,7 +14,7 @@ int main(void)
 	for (; x < 100; x++)
 	{
 		if (x % 3 == 0 && x % 5 == 0)
-		       printf("FizzBuzz ");
+			printf("FizzBuzz ");
 		else if (x % 3 == 0)
 			printf("Fizz ");
 		else if (x % 5 == 0)
@@ -22,7 +22,7 @@ int main(void)
 		else
 			printf("%d ", x);
 	}
-	printf("Buzz\n");
+			printf("Buzz\n");
 
 	return (0);
 }

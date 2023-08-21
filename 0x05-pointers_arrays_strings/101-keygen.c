2cfa468446687484f626c5dd8361d44e59f81cd8
SegunEmmanuel1 <segunemmanuel67@yahoo.com>
2023-08-21 14:18:05 -0700

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -13,14 +13,14 @@ int main(void)
 	time_t t;
 
 	srand((unsigned int) time(&t));
-	while (j < 2525)
+	while (j < 2772)
 	{
 		i = rand() % 128;
-		if ((j + i) > 2525)
+		if ((j + i) > 2772)
 			break;
 		j = j + i;
 		printf("%c", i);
 	}
-	printf("%c\n", (2525 - j));
+	printf("%c\n", (2772 - j));
 	return (0);
-}
+}}
